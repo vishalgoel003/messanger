22876072e843b247656cf9f5015cd06849880136
vishalgoel003 <vishal.goel@novopay.in>
2017-01-27 18:06:23 +0530

    2.9 completed

--- src/main/java/in/kirancity/jersey/messanger/resources/MessageResource.java
+++ src/main/java/in/kirancity/jersey/messanger/resources/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,9 +17,16 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Message getMessage(@PathParam("messageId") Long id){
+		return messageService.getMessage(id);
 	}
 	
 }
--- src/main/java/in/kirancity/jersey/messanger/service/MessageService.java
+++ src/main/java/in/kirancity/jersey/messanger/service/MessageService.java
@@ -2,18 +2,44 @@ package in.kirancity.jersey.messanger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import in.kirancity.jersey.messanger.database.DatabaseClass;
 import in.kirancity.jersey.messanger.model.Message;
 
 public class MessageService {
 	
-	public List<Message> getAllMessages(){
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService(){
 		Message m1 = new Message(1L, "Hello world", "Kaushik");
 		Message m2 = new Message(2L, "Hello Jersey", "Kaushik");
-		List<Message> result = new ArrayList<>();
-		result.add(m1);
-		result.add(m2);
-		return result;
+		messages.put(m1.getId(), m1);
+		messages.put(m2.getId(), m2);
 	}
-
+	
+	public List<Message> getAllMessages(){
+		return new ArrayList<>(messages.values());
+	}
+	
+	public Message getMessage(Long id){
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message){
+		message.setId(messages.size()+1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message){
+		if(message.getId()<=0)
+			return null;
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id){
+		return messages.remove(id);
+	}
 }
